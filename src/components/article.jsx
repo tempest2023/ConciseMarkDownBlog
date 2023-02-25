76dc8c0fa80b6c7effd8ac5afdbded25f5d2f610
Tempest <623059008@qq.com>
2023-02-25 15:45:35 -0500

    feat: use icon on filpbutton

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -16,6 +16,7 @@ import FlipButton from './FlipButton';
 import MarkdownTextarea from './editor/markDownTextarea';
 import ColorLoading from './colorLoading';
 import NotFound from '../articles/404.md';
+import { codeIcon, paragraphIcon } from '../util/icons';
 
 const { debug } = config;
 
@@ -61,7 +62,12 @@ const Article = () => {
         ? (
           <div>
             <div className={styles['top-right-button']}>
-              <FlipButton onClick={switchMode} open={false} closeElement={'View'} openElement={'Raw'} size="small" />
+              <FlipButton
+                onClick={switchMode}
+                open={false}
+                closeElement={<img className={styles['small-icon']} src={paragraphIcon} />}
+                openElement={<img className={styles['small-icon']} src={codeIcon} />}
+                size="small" />
             </div>
             {mode !== 'preview' && <MarkdownTextarea showHeader={false} deafultValue={markdownContent} updatePreview={() => {}} />}
             {mode === 'preview' &&
